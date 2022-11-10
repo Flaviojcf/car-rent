f212957329213687ce5a2fc37df186d1fc8f4b1f
Flavio <fjcf@poli.br>
2022-11-10 16:52:49 -0300

    feat: create index for routes

--- src/routes/specification.routes.ts
+++ src/routes/specification.routes.ts
@@ -1,18 +1,10 @@
-import { SpecificationRepository } from "../modules/cars/repositories/implementations/SpecificationRepository";
-import { CreateSpecificationService } from "../modules/cars/services/CreateSpecificationService";
-
 import { Router } from "express";
+
+
+import { createSpeficiationController } from "../modules/cars/useCases/createSpecification";
 
 export const specificationsRoutes = Router();
 
-const specificationRepository = new SpecificationRepository();
-
 specificationsRoutes.post("/", (request, response) => {
-  const { name, description } = request.body;
-  const createSpecificationService = new CreateSpecificationService(
-    specificationRepository
-  );
-
-  createSpecificationService.execute({ name, description });
-  return response.status(201).send();
+  return createSpeficiationController.handle(request, response);
 });
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,12 @@
 import express from "express";
-import { categoriesRoutes } from "./routes/categories.routes";
-import { specificationsRoutes } from "./routes/specification.routes";
+import { router } from "./routes";
+
 
 const app = express();
 
 app.use(express.json());
+app.use(router)
 
-app.use("/categories", categoriesRoutes);
-app.use("/specifications", specificationsRoutes);
+
 
 app.listen(3333, ()=>console.log('Sever is running'));
